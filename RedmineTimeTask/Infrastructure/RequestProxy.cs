ee5d3ad1dada59d394c34a2c63b9eabcea8f1e74
oskarp <oskarpttersson@gmail.com>
2015-08-23 11:29:16 +0200

    Working, now time for refactoring stage 1

--- RedmineTimeTask/Infrastructure/RequestProxy.cs
+++ RedmineTimeTask/Infrastructure/RequestProxy.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 using Redmine.Net.Api;
 using Redmine.Net.Api.Types;
 using System.Collections.Specialized;
+using RedmineTimeTask.Models;
 
 namespace RedmineTimeTask.Infrastructure
 {
     class RequestProxy
     {
         IList<Issue> _issues;
+        IList<TimeEvent> _timeevents = new List<TimeEvent>();
+        RedmineManager manager;
 
         public IList<Issue> Issues
         {
@@ -26,15 +29,47 @@ namespace RedmineTimeTask.Infrastructure
                 _issues = value;
             }
         }
-        
+        public void createManager()
+        {
+            if (Properties.Settings.Default.Host.Length != 0 && Properties.Settings.Default.APIkey.Length != 0) {   
+                this.manager = new RedmineManager(Properties.Settings.Default.Host, Properties.Settings.Default.APIkey);
+            }
+        }
         public void fetchIssues()
         {
-            if(Properties.Settings.Default.Host.Length != 0 && Properties.Settings.Default.APIkey.Length != 0) { 
-                var manager = new RedmineManager(Properties.Settings.Default.Host, Properties.Settings.Default.APIkey);
-                
-                var parameters = new NameValueCollection { { "assigned_to_id", "*" } };
-                parameters.Add("status_id", "open");
-                this.Issues = manager.GetObjectList<Issue>(parameters);
+            if(this.manager != null) {
+                try
+                {
+                    var parameters = new NameValueCollection { { "assigned_to_id", "me" } };
+                    parameters.Add("status_id", "open");
+                    this.Issues = manager.GetObjectList<Issue>(parameters);
+                    TimeEntry t = new TimeEntry();
+                } 
+                catch (RedmineException e)
+                {
+                    Console.WriteLine("ERROR : " + e.Message);
+                }
+
+            }
+        }
+       
+        public void submitTime(TimeEvent te)
+        {
+            _timeevents.Add(te);
+            /*if(te.Issue.SpentHours != null)
+            {
+                te.Issue.SpentHours = te.Issue.SpentHours + (float)Math.Round((decimal)te.Timespan.TotalHours, 2);
+            } else
+            {
+                te.Issue.SpentHours = (float)Math.Round((float)te.Timespan.TotalHours, 2);
+            } */
+            te.Issue.SpentHours = te.Issue.SpentHours + 2.50F;
+            try
+            { 
+            manager.UpdateObject(te.Issue.Id.ToString(), (Issue)te.Issue);
+            } catch (RedmineException e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
--- RedmineTimeTask/MainWindow.xaml.cs
+++ RedmineTimeTask/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using Redmine.Net.Api;
 using Redmine.Net.Api.Types;
 using System.Collections;
 using RedmineTimeTask.Infrastructure;
+using RedmineTimeTask.Models;
 
 namespace RedmineTimeTask
 {
@@ -28,12 +29,14 @@ namespace RedmineTimeTask
     public partial class MainWindow : Window
     {
         RequestProxy rp = new RequestProxy();
+        Timer timer = new Timer();
 
         public MainWindow()
         {
             InitializeComponent();
 
             // Put on its own thread later. 
+            rp.createManager();
             rp.fetchIssues();
             issues_ListBox.ItemsSource = rp.Issues;
 
@@ -52,11 +55,26 @@ namespace RedmineTimeTask
 
         private void startWatch_Menu(object sender, RoutedEventArgs e)
         {
-            
+            //Console.WriteLine(sender.ToString());
+            MenuItem item = (System.Windows.Controls.MenuItem)sender;
+            if((string)item.Header == "_Start Timer") {
+                item.Header = "_Stop Timer";
+                this.timer.start();
+            } else if((string)item.Header == "_Stop Timer")
+            {
+                SubmitTime subWindow = new SubmitTime();
+                subWindow.Rp = this.rp;
+                subWindow.Te = new TimeEvent(timer.getTimeSpan(), null);
+                subWindow.Show();
+                item.Header = "_Start Timer";
+            }
+
+
         }
 
         private void ForceRefresh_Menu(object sender, RoutedEventArgs e)
         {
+            rp.createManager();
             rp.fetchIssues();
             issues_ListBox.ItemsSource = rp.Issues;
 
